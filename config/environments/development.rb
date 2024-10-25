2202910e43f5eca1e55058700f9e34d05c9cc138
hoainam1111 <namtre0712@gmail.com>
2024-10-25 15:53:42 +0000

    aaaa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "https://learning-management-pb0f.onrender.com" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
